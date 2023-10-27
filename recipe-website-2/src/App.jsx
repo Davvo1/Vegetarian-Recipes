cb1dfe50164bfcd8011ddfe6678fe64bb83b3700
Davvo1 <davidebonardi15@gmail.com>
2023-10-27 12:31:25 +0200

    other tweaks to main page style

--- recipe-website-2/src/App.jsx
+++ recipe-website-2/src/App.jsx
@@ -2,10 +2,7 @@ import './App.css';
 import { useState, useEffect, useRef } from 'react';
 import RecipeCard from './Recipe';
 import loadingGif from './assets/loading.gif';
-import {Routes, Route, useNavigate} from 'react-router-dom';
-import RecipeDetail from './RecipeDetail';
-import SearchBar from './SearchBar';
-
+import chefHat from "./assets/chef.png";
 
 function App() {
 
@@ -14,9 +11,10 @@ function App() {
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
+  const [searched, setSearched] = useState(false);
   const isInitialMount = useRef(true);
-  
- useEffect(() => {
+
+  useEffect(() => {
     if (isInitialMount.current) {
       isInitialMount.current = false;
       return;
@@ -28,17 +26,17 @@ function App() {
 
   async function getRecipes() {
     try {
-    let response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&diet=vegetarian`);
-    if(!response.ok) {
-      throw new Error('Network response was not ok');
-    } 
-    let data = await response.json();
-    setRecipes(data.results);
-    console.log(data.results);
-    } catch(err) {
-    console.log(err)
-    } finally {    
-    setLoading(false);
+      let response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&diet=vegetarian`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      let data = await response.json();
+      setRecipes(data.results);
+      console.log(data.results);
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -47,50 +45,49 @@ function App() {
   }
 
   function submitSearch(e) {
-      e.preventDefault()
-      setQuery(search);
-      if(query === '') {
-      return;
+    e.preventDefault()
+    setQuery(search);
+    if (query !== search) {
+      setSearch('');
+      console.log(query);
+      setLoading(true);
+      setSearched(true);
+      console.log(searched);
     }
-    if(search === query) {
-      return;
-    }  
-    console.log(err);
-    setSearch('');
-    console.log(query);
-     setLoading(true);
+
   }
 
 
   return (
     <div className="App">
       <form className="search-form"
-      onSubmit={submitSearch}>
+        onSubmit={submitSearch}>
         <input
-        className='search-bar'
-        type="text"
-        value={search}
-        placeholder='What do you want to cook today?'
-        onChange={updateSearch}
+          className='search-bar'
+          type="text"
+          value={search}
+          placeholder='What do you want to cook today?'
+          onChange={updateSearch}
         />
       </form>
-      <p className="introduction">Recommended recipes</p>
+      <p className="introduction"><img src={chefHat} className='chef-hat'/>{searched ? "Searched Recipes" : "Recommended recipes"}</p>
       {loading ? <img className='loading-gif'
-      src={loadingGif}/> : 
-      <div className='recipe-holder'>
-      {recipes.map(recipe => {
-        return (
-          <>
-        <RecipeCard 
-        title={recipe.title}
-        image={recipe.image}
-        key={recipe.id}
-        id={recipe.id}
-        />
-        </>
-      )})}
+        src={loadingGif} /> :
+        <div className='recipe-holder'>
+          {recipes.map(recipe => {
+            return (
+              <>
+                <RecipeCard
+                  title={recipe.title}
+                  image={recipe.image}
+                  key={recipe.id}
+                  id={recipe.id}
+                />
+              </>
+            )
+          })}
 
-      </div>}
+        </div>}
     </div>
   );
 
